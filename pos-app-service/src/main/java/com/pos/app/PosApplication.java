cb11c991c1f0f5bf645d0e6178f5b92389b55a1e
JINESHBHASKAR <jineshbhaskarkp@gmail.com>
2022-11-05 13:01:15 +0530

    file uploading completed

--- pos-app-service/src/main/java/com/pos/app/PosApplication.java
+++ pos-app-service/src/main/java/com/pos/app/PosApplication.java
@@ -2,8 +2,14 @@ package com.pos.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.pos.app.config.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageProperties.class
+})
 public class PosApplication {
 
 	public static void main(String[] args) {
